95ed5183476205dd064917a8d9545186a6ccdc1d
imkf-zhang <15638452918@163.com>
2021-10-14 13:59:36 +0800

    [功能]组件样式重叠问题处理，学习css module

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { NavBar } from "antd-mobile";
 import PropTypes from "prop-types";
-import "./index.css";
+import styles from "./index.module.css";
+// import "./index.css";
 function NavHeader(props) {
   const defaultHandler = () => { props.history.go(-1)};
   return (
     <NavBar
       mode="light"
+      className={styles.navBar}
       icon={<i className="iconfont icon-back" />}
       onLeftClick={ props.onLeftClick || defaultHandler }
     >
