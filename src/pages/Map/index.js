be17e6a7239d8d0da887b23d53e1d3735df74fa5
imkf-zhang <15638452918@163.com>
2021-10-14 18:16:34 +0800

    [功能]渲染区级房源覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,23 +1,93 @@
 import React from "react";
+import axios from "axios"
 import NavHeader from "../../components/NavHeader";
-import "./index.css";
+import styles from "./index.module.css"
+
+const BMap = window.BMap;
+const labelStyle = {
+  cursor: 'pointer',
+  border: '0px solid rgb(255, 0, 0)',
+  padding: '0px',
+  whiteSpace: 'nowrap',
+  fontSize: '12px',
+  color: 'rgb(255, 255, 255)',
+  textAlign: 'center'
+}
+const { label, value } = JSON.parse(localStorage.getItem("hkzf_city"));
 class Map extends React.Component {
   // 容器准备好了再进行初始化地图实例
   componentDidMount() {
-      // 创建地图实例
-      const map = new window.BMap.Map('container')
-      // 设置中心点
-      const point = new window.BMap.Point(116.404,39.915)
-      // 地图初始化，同时设置地图展示级别
-      map.centerAndZoom(point, 15);  
+    this.initMap();
+    
   }
+  initMap = () => {
+    // 创建地图实例
+    const map = new BMap.Map("container");
+    // 创建地址解析器实例
+    let myGeo = new BMap.Geocoder();
+    // 将地址解析结果显示在地图上，并调整地图视野
+    myGeo.getPoint(
+      label,
+      (point) => {
+        if (point) {
+          map.centerAndZoom(point, 11);
+          map.addControl(new BMap.NavigationControl());
+          map.addControl(new BMap.ScaleControl());
+        }
+      },
+      label
+    );
+    this.getRange(map);
+  };
+  /**
+   * 绘制文本标注
+   * @param {Object} map 
+   * @param {Object} item 
+   */
+  textAnnotation = (map,item) => {
+    var point = new BMap.Point(Number(item.coord.longitude), Number(item.coord.latitude));
+    let opts = {
+      position: point, // 指定文本标注所在的地理位置
+      offset: new BMap.Size(-35, -35), // 设置文本偏移量
+    };
+    // 创建文本标注对象
+    let labeL = new BMap.Label("", opts);
+    labeL.setContent(`<div class="${styles.bubble}">
+    <p class="${styles.name}">${item.label}</p>
+    <p>${item.count}套</p>
+    </div>`);
+    labeL.addEventListener('click',()=> {
+      console.log(item.value);
+      map.centerAndZoom(point, 16);
+      setTimeout(()=> {
+        map.clearOverlays()
+      },0);
+    })
+    labeL.setStyle(labelStyle);
+    labeL.id = item.value
+    map.addOverlay(labeL);
+   }
+  /** 
+   * 获取区的数据
+   * @param { Object } map
+   * @returns 
+   */
+  async getRange(map) {
+    const { data: res } = await axios.get("http://localhost:8080/area/map", {
+      params: {
+        id: value,
+      },
+    });
+    res.body.forEach(item => {
+         this.textAnnotation(map,item)
+    })
+  }
+
   render() {
     return (
-      <div className="map">
-        <NavHeader>
-          地图找房
-        </NavHeader>
-        <div id="container"></div>
+      <div className={styles.map}>
+        <NavHeader>地图找房</NavHeader>
+        <div id="container" className={styles.container}></div>
       </div>
     );
   }
