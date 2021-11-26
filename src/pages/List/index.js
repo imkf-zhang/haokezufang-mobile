ed14e226b84ebec7e0031741f978b3dd45539ee3
imkf-zhang <15638452918@163.com>
2021-11-26 10:48:58 +0800

    [功能]InfiniteLoader组件的使用，实现无限滚动加载列表数据

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -3,75 +3,105 @@ import { Flex } from "antd-mobile";
 import SearchHeader from "../../components/SearchHeader";
 import Filter from "./components/Filter/index";
 import styles from "../List/index.module.css";
-import API from "../../utils/api"
-import { AutoSizer, List, WindowScroller } from "react-virtualized";
+import API from "../../utils/api";
+import {
+  AutoSizer,
+  List,
+  WindowScroller,
+  InfiniteLoader,
+} from "react-virtualized";
 import HouseItem from "../../components/HouseItem";
-import { BASE_URL } from "../../utils/url"
+import { BASE_URL } from "../../utils/url";
 
 const { label } = JSON.parse(localStorage.getItem("hkzf_city"));
 
 class News extends React.Component {
   state = {
     list: [],
-    count: 0
+    count: 0,
   };
   filters = {};
   componentDidMount() {
-    this.searchHouseList()
+    this.searchHouseList();
   }
   /**
    * 接受Fillter组件中中的筛选数据
-   * @param { Object} 
+   * @param { Object}
    */
   onFilter = (filters) => {
     this.filters = filters;
-    this.searchHouseList()
+    this.searchHouseList();
   };
   /**
    * 发送请求，获取房屋筛选数据
-   * @returns 
+   * @returns
    */
-   searchHouseList = async () => {
+  searchHouseList = async () => {
     // 获取城市id
-    const { value } = JSON.parse(localStorage.getItem('hkzf_city'));
-    const {data: { body: { list,count} } } = await API.get('/houses',{
+    const { value } = JSON.parse(localStorage.getItem("hkzf_city"));
+    const {
+      data: {
+        body: { list, count },
+      },
+    } = await API.get("/houses", {
       params: {
         cityId: value,
         ...this.filters,
         start: 1,
-        end: 20
-      }
-    })
+        end: 20,
+      },
+    });
     this.setState(() => {
       return {
         list,
-        count
-      }
-    })
-    console.log("获取房屋筛选数据",list,count)
+        count,
+      };
+    });
+    console.log("获取房屋筛选数据", list, count);
   };
   /**
    * 渲染房屋详情列表
-   * @returns 
+   * @returns
    */
-   renderHouseList = ({
+  renderHouseList = ({
     key, // Unique key within array of rows key值，唯一值
     index, // Index of row within collection 索引
     style, // 重点属性，一定要给每一行数据添加样式，作用，指定每一行位置。
-   }) => {
-     const { list } = this.state;
-     const house = list[index];
-     console.log(house)
-    return (<HouseItem 
-      key={key} 
-      style={style} 
-      src={BASE_URL + house.houseImg}
-      title={ house.title}
-      desc={house.desc}
-      tags={house.tags}
-      price={house.price}
-       />)
-   }
+  }) => {
+    const { list } = this.state;
+    const house = list[index];
+    console.log(house);
+    return (
+      <HouseItem
+        key={key}
+        style={style}
+        src={BASE_URL + house.houseImg}
+        title={house.title}
+        desc={house.desc}
+        tags={house.tags}
+        price={house.price}
+      />
+    );
+  };
+  /**
+   * 判断列表中的每一行是否加载完成
+   * @param { Number }  index
+   * @returns { Boolean } 是否加载完成
+   */
+  isRowLoaded = ({ index }) => {
+    return !!this.state.list[index];
+  };
+  /**
+   * 用来获取更多房屋列表数据
+   * @param { Number Number }  startIndex stopIndex
+   * @returns { Promise } 在数据完成加载时调用resolve
+   */
+  loadMoreRows = ({ startIndex, stopIndex }) => {
+    console.log(startIndex, stopIndex)
+    return new Promise( resolve => {
+      //数据加载完成时调用resolve这个方法即可 
+    })
+  };
   render() {
     return (
       <div className={styles.root}>
@@ -90,26 +120,33 @@ class News extends React.Component {
         <Filter onFilter={this.onFilter}></Filter>
         <div className={styles.houseItems}>
           {/* list自身有滚动条这个并不是想要的，想要其跟随页面滚动 */}
-            <WindowScroller>
-              {
-                ({height,isScrolling,scrollTop}) => (
+          <InfiniteLoader
+            isRowLoaded={this.isRowLoaded}
+            loadMoreRows={this.loadMoreRows}
+            rowCount={this.state.count}
+            minimumBatchSize={20}
+          >
+            {({ onRowsRendered, registerChild }) => (
+              <WindowScroller>
+                {({ height, isScrolling, scrollTop }) => (
                   <AutoSizer>
-                    {
-                      ({width}) => (
-                        <List
+                    {({ width }) => (
+                      <List
+                        onRowsRendered={onRowsRendered}
+                        ref={registerChild}
                         autoHeight
                         width={width}
                         height={height}
                         rowCount={this.state.count}
                         rowHeight={120}
                         rowRenderer={this.renderHouseList}
-                       />
-                      )
-                    }
+                      />
+                    )}
                   </AutoSizer>
-                )
-              }
-            </WindowScroller>
+                )}
+              </WindowScroller>
+            )}
+          </InfiniteLoader>
         </div>
       </div>
     );
