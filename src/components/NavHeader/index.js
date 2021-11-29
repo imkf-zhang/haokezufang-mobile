3f267be92b8418b937a1fd18c2f0dc1ab3b8d426
imkf-zhang <15638452918@163.com>
2021-11-29 17:57:38 +0800

    [功能]房屋详情页面的完成、路由参数的使用：传参和接收参数

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,25 +1,48 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import { NavBar } from "antd-mobile";
-import PropTypes from "prop-types";
-import styles from "./index.module.css";
-// import "./index.css";
-function NavHeader(props) {
-  const defaultHandler = () => { props.history.go(-1)};
+import React from 'react'
+
+import { NavBar } from 'antd-mobile'
+
+// 导入 withRouter 高阶组件
+import { withRouter } from 'react-router-dom'
+
+// 导入 props 校验的包
+import PropTypes from 'prop-types'
+
+// 导入样式
+// import './index.scss'
+import styles from './index.module.css'
+
+// 添加 className 和 rightContent（导航栏右侧内容） 属性
+function NavHeader({
+  children,
+  history,
+  onLeftClick,
+  className,
+  rightContent
+}) {
+  // 默认点击行为
+  const defaultHandler = () => history.go(-1)
+
   return (
     <NavBar
+      className={[styles.navBar, className || ''].join(' ')}
       mode="light"
-      className={styles.navBar}
       icon={<i className="iconfont icon-back" />}
-      onLeftClick={ props.onLeftClick || defaultHandler }
+      onLeftClick={onLeftClick || defaultHandler}
+      rightContent={rightContent}
     >
-      {props.children}
+      {children}
     </NavBar>
-  );
+  )
 }
+
+// 添加props校验
 NavHeader.propTypes = {
-    children: PropTypes.string.isRequired,
-    onLeftClick: PropTypes.func
-  }
-// withRouter(NavHeader) 返回值也是一个组件
-export default withRouter(NavHeader);
+  children: PropTypes.string.isRequired,
+  onLeftClick: PropTypes.func,
+  className: PropTypes.string,
+  rightContent: PropTypes.array
+}
+
+// withRouter(NavHeader) 函数的返回值也是一个组件
+export default withRouter(NavHeader)
