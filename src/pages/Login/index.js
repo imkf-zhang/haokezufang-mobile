468d6570230ebe799fa4d082704c64298753edf9
imkf-zhang <15638452918@163.com>
2021-11-30 16:44:06 +0800

    [功能]登陆功能的完整实现->利用formik进行表单操作，以及利用yup进行表单验证

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,89 +1,59 @@
-import React, { Component } from 'react'
-import { Flex, WingBlank, WhiteSpace, Toast } from 'antd-mobile'
+import React, { Component } from "react";
+import { Flex, WingBlank, WhiteSpace, Toast } from "antd-mobile";
 
-import { Link } from 'react-router-dom'
-import API from '../../utils/api'
+import { Link } from "react-router-dom";
+import { withFormik } from "formik";
+import * as yup from 'yup';
 
-import NavHeader from '../../components/NavHeader'
+import API from "../../utils/api";
 
-import styles from './index.module.css'
+import NavHeader from "../../components/NavHeader";
 
+import styles from "./index.module.css";
 // 验证规则：
-// const REG_UNAME = /^[a-zA-Z_\d]{5,8}$/
-// const REG_PWD = /^[a-zA-Z_\d]{5,12}$/
-
+const REG_UNAME = /^[a-zA-Z_\d]{5,8}$/
+const REG_PWD = /^[a-zA-Z_\d]{5,12}$/
 class Login extends Component {
-  state = {
-    username: "",
-    password: ""
-  }
-  /**
-   * 使用受控组件，使状态和form组件的value双向绑定
-   */
-  getUserName = (e) => {
-    this.setState( ()=> {
-      return {
-        username: e.target.value
-      }
-    })
-  }
-  /**
-   * 使用受控组件，使状态和form组件的value双向绑定
-   */
-  getPassword = e => {
-    this.setState( ()=> {
-      return {
-        password: e.target.value
-      }
-    })
-  }
-  /**
-   * 表单的提交事件的处理事件
-   */
-  handleSubmit = async (e) => {
-    // 表单的默认提交事件会刷新页面->需要阻止其默认行为
-    e.preventDefault();
-    const { username,password } = this.state;
-    const { data: { body,description,status}} = await API.post('/user/login',{username,password});
-    if (status === 200) {
-      localStorage.setItem('hkzf_token',body.token);
-      this.props.history.go(-1);
-    }else {
-      Toast.info(description, 2 , null,false);
-    }
-  }
   render() {
-    const { username,password } = this.state;
+    // 通过props获取高阶组件传递进来的属性
+    const { values, handleChange, handleSubmit, touched, errors, handleBlur } = this.props;
+    // 尝试打印一下接收来的高阶组件的值
+    // console.log(values,handleChange,handleSubmit)
+    console.log(touched, errors)
     return (
       <div className={styles.root}>
         {/* 顶部导航 */}
         <NavHeader className={styles.navHeader}>账号登录</NavHeader>
         <WhiteSpace size="xl" />
-
         {/* 登录表单 */}
         <WingBlank>
-          <form onSubmit = {this.handleSubmit}>
+          {/* FIXME:两个input框使用的是一个onChange函数，就是因为有了name属性的原因，name属性必须提供并且和values对象的属性一样（受控组件） */}
+          <form onSubmit={handleSubmit}>
             <div className={styles.formItem}>
               <input
                 className={styles.input}
-                value={username}
-                onChange={this.getUserName}
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
                 name="username"
                 placeholder="请输入账号"
               />
             </div>
+            {/* 利用的是短路运算符 */}
+            {errors.username && touched.username && <div className={styles.error}>{errors.username}</div>}
             {/* 长度为5到8位，只能出现数字、字母、下划线 */}
-            {/* <div className={styles.error}>账号为必填项</div> */}
             <div className={styles.formItem}>
               <input
                 className={styles.input}
-                value={password}
-                onChange={this.getPassword}
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
                 name="password"
                 type="password"
                 placeholder="请输入密码"
               />
             </div>
+            {errors.password && touched.password && <div className={styles.error}>{errors.password}</div>}
             {/* 长度为5到12位，只能出现数字、字母、下划线 */}
             {/* <div className={styles.error}>账号为必填项</div> */}
             <div className={styles.formSubmit}>
@@ -99,8 +69,45 @@ class Login extends Component {
           </Flex>
         </WingBlank>
       </div>
-    )
+    );
   }
 }
 
-export default Login
+// 使用withFormik高阶组件包装login组件
+Login = withFormik({
+  // 提供状态
+  mapPropsToValues: () => ({ username: "", password: "" }),
+  // validate: values => {
+  //    const errors = {};
+  //    if (!values.name) {
+  //      errors.name = 'Required';
+  //    }
+  //    return errors;
+  //  },
+  // 表单验证
+  validationSchema: yup.object().shape({
+    username: yup.string().required('账号为必填项').matches(REG_UNAME,'长度为5到8位，只能出现数字、字母、下划线'),
+    password: yup.string().required('密码为必填项').matches(REG_PWD,'长度为5到8位，只能出现数字、字母、下划线'),
+  }),
+  // 表单的提交事件
+  handleSubmit: async (values,{props}) => {
+    console.log("values", values);
+    const {
+      data: { body, description, status },
+    } = await API.post("/user/login", {
+      username: values.username,
+      password: values.password,
+    });
+    if (status === 200) {
+      localStorage.setItem("hkzf_token", body.token);
+      // 注意：无法在该方法中，通过this来获取到路由信息
+      // 所以通过第二个对象参数中获取到props来使用props（参数props指的就是被包裹组件的props）
+      // this.props.history.go(-1);
+      props.history.go(-1);
+    } else {
+      Toast.info(description, 2, null, false);
+    }
+  },
+})(Login);
+
+export default Login;
