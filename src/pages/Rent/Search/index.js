6d71dacd81fb8463871b7f78b7d0409c08cf34e6
imkf-zhang <15638452918@163.com>
2021-12-02 22:46:21 +0800

    [功能]搜索房源信息页面，获取搜索到的信息，渲染成功

--- src/pages/Rent/Search/index.js
+++ src/pages/Rent/Search/index.js
@@ -1,35 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { SearchBar } from 'antd-mobile'
+import { SearchBar } from "antd-mobile";
 
-import { getCity } from '../../../utils/city'
+import { getCity } from "../../../utils/city";
+import API from "../../../utils/api";
+import styles from "./index.module.css";
 
-import styles from './index.module.css'
 
 export default class Search extends Component {
   // 当前城市id
-  cityId = getCity().value
+  cityId = getCity().value;
 
   state = {
     // 搜索框的值
-    searchTxt: '',
-    tipsList: []
-  }
+    searchTxt: "",
+    tipsList: [],
+  };
 
   // 渲染搜索结果列表
   renderTips = () => {
-    const { tipsList } = this.state
+    const { tipsList } = this.state;
 
-    return tipsList.map(item => (
+    return tipsList.map((item) => (
       <li key={item.community} className={styles.tip}>
         {item.communityName}
       </li>
-    ))
-  }
-
+    ));
+  };
+  handleSearchTxt = async (value) => {
+    this.setState({
+      searchTxt: value,
+    });
+    if (!value) {
+      return this.setState({
+        tipsList: []
+      });
+    } else {
+      const { data: { body} } = await API.get("/area/community",{
+        params: {
+          name: value,
+          id: this.cityId
+        },
+      });
+     this.setState({
+       tipsList: body
+     })
+    }
+  };
   render() {
-    const { history } = this.props
-    const { searchTxt } = this.state
+    const { history } = this.props;
+    const { searchTxt } = this.state;
 
     return (
       <div className={styles.root}>
@@ -37,13 +57,14 @@ export default class Search extends Component {
         <SearchBar
           placeholder="请输入小区或地址"
           value={searchTxt}
+          onChange={this.handleSearchTxt}
           showCancelButton={true}
-          onCancel={() => history.replace('/rent/add')}
+          onCancel={() => history.replace("/rent/add")}
         />
 
         {/* 搜索提示列表 */}
         <ul className={styles.tips}>{this.renderTips()}</ul>
       </div>
-    )
+    );
   }
 }
